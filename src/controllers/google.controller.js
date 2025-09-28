ec0d90efeb391a036d68fd17b550e1cd7e4cab4d
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-09-28 23:08:51 +0700

    fix: google login

--- src/controllers/google.controller.js
+++ src/controllers/google.controller.js
@@ -1,53 +1,12 @@
-import { OAuth2Client } from 'google-auth-library'
-import prisma from "../db/index.js"
-import jwt from "jsonwebtoken"
+import { googleLogin } from '../services/google.services.js';
 
 export const google = async (req, res) => {
-       
-
-    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
     try {
 
         const {token} = req.body;
 
-
-        const ticket = await client.verifyIdToken({
-          idToken : token,
-          audience : process.env.GOOGLE_CLIENT_ID
-
-        })
-
-        const payload = ticket.getPayload();
-        const {email, name, avatar} = payload;
-
-
-        let user = await prisma.users.findUnique({where : {email}});
-const defaultAvatar = "https://res.cloudinary.com/demo/image/upload/v123456789/avatar.png";
-
-        if(!user) {
-
-            user = await prisma.users.create({
-           data :
-
-           {
-            fullName : name, 
-            email,
-            phone : null,
-           avatar: avatar || defaultAvatar,
-          password: null,
-          isVerified: true,
-           }
-                
-            })
-        }
-
-        const appToken = jwt.sign(
-            {id : user.id, email : user.email},
-            process.env.JWT_SECRET,
-            {expiresIn : "1h"}
-        )
-
-        res.json({message : "Login Successful", token : appToken, user})
+        const newToken = await googleLogin({token})
+        res.json({message : "Login Successful", newToken})
         
     } catch (error) {
         return res.status(500).json({message : error.message})
