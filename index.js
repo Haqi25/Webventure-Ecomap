28b23c4530ed3b49d23165bc14cefef6639d5463
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-03 08:22:05 +0700

    feat: add category

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import review from "./src/routes/review.routes.js"
 import { adminMiddleware } from "./src/middlewares/admin.middleware.js";
 import { authMiddleware } from "./src/middlewares/auth.middleware.js";
 import profile from "./src/routes/profile.routes.js"
+import getData from "./src/routes/admin/dashboard.routes.js"
 
 const app = express();
 
@@ -17,11 +18,17 @@ app.get('/', (req, res) => {
 res.send('Hello World!');
 })
 
+//Consumer
 app.use("/api/auth", authUser);
 app.use("/api/umkm", umkm);
 app.use("/api/admin/category", adminMiddleware, category)
 app.use("/api/profile", authMiddleware, profile)
 app.use("/api/review", authMiddleware, review )
+
+
+//Admin
+
+app.use("/api/admin", adminMiddleware, getData)
 
 const PORT = process.env.PORT || 3000;
 
