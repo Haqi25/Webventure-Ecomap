19134e7b67363c6fab6f79ea75b440da2e6331b8
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-03 23:16:54 +0700

    feat: create category

---
+++ src/admin/controllers/category.controller.js
@@ -0,0 +1,17 @@
+import { categoryStore } from "../services/category.services.js";
+
+
+export const store = async (req, res) => {
+
+    try {
+        
+        const {name, slug, icon, description} = req.body;
+
+        const newCategory = await categoryStore({name, slug, icon, description})
+        res.json({message : "Kategori Berhasil dibuat", newCategory})
+    } catch (error) {
+        return res.status(500).json({error : error.message})
+    }
+}
+
+
