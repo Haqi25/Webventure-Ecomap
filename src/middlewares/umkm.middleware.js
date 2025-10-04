45b6639d80872e14ea2b3479811cef138e3db0b3
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-05 01:10:13 +0700

    fix umkmmiddleware

--- src/middlewares/umkm.middleware.js
+++ src/middlewares/umkm.middleware.js
@@ -3,27 +3,31 @@ import jwt from "jsonwebtoken";
 
 
 export const umkmMiddleware = async (req, res, next) => {
-     const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers["authorization"]?.split(" ")[1];
 
-     if(!token) {
-        return res.status(401).json({ error : "No Token Provided"})
-     }
+  if (!token) {
+    return res.status(401).json({ error: "No Token Provided" });
+  }
 
-     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const role = await prisma.users.findUnique({
-            where: { id : decoded.id}
-        })
+    const user = await prisma.users.findUnique({
+      where: { id: decoded.id },
+    });
 
-        if(!role){
-            return res.status(404).json({ error : "User Tidak Ditemukan"})
-        }
+    if (!user) {
+      return res.status(404).json({ error: "User Tidak Ditemukan" });
+    }
 
-        if(role.role !== "UMKM"){
-            return res.status(400).json({ error : "url tidak ditemukan"})
-        }
-     } catch (error) {
-        return res.status(403).json({error : "Token expired"})
-     }
-}
+    if (user.role !== "UMKM") {
+      return res.status(400).json({ error: "url tidak ditemukan" });
+    }
+
+    req.userId = user.id;       
+    req.userRole = user.role;   
+    next();
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
