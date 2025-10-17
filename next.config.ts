070b13b9d6d8edbf2b74d275c13e9fd3f98e8867
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-17 18:26:17 +0700

    chore: Images Domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    domains: ['localhost'], // tambahkan domain host gambar
+  },
 };
 
 export default nextConfig;
